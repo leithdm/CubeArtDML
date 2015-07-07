f137c18898cd69ca0ac5d57dea77a6105eb390f7
Darren Martin Leith <leithdm@gmail.com>
2015-07-07 21:12:16 +0100

    Add deployment file

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -18,18 +18,18 @@ return array(
 	|
 	*/
 
-//	'connections' => array(
-//
-//		'mysql' => array(
-//			'driver'    => 'mysql',
-//			'host'      => 'localhost',
-//			'database'  => 'art',
-//			'username'  => 'root',
-//			'password'  => 'root',
-//			'charset'   => 'utf8',
-//			'collation' => 'utf8_unicode_ci',
-//			'prefix'    => '',
-//		)
-//	),
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'art',
+			'username'  => 'root',
+			'password'  => 'root',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		)
+	),
 
 );
